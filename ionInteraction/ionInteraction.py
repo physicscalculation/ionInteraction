f992ceddbee531165f705eeed7569aa8c474c543
physicscalculation <167899609+physicscalculation@users.noreply.github.com>
2024-09-29 01:15:36 +0300

    Add Windows download directory information

--- ionInteraction/ionInteraction.py
+++ ionInteraction/ionInteraction.py
@@ -28,6 +28,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.N_AVAGADRO = 6.02214076e23
         self.data_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
         self.results_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Results')
+        # self.results_folder = os.path.join('C:\\', 'Results') # Change this line for Windows app
 
 
 
